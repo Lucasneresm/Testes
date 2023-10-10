3c6e18035e518753283ce72a7a30be2dd648c0ff
lucas neres mota <lucasneresm@hotmail.com>
2023-10-09 23:00:32 -0300

    parametros em teste

---
+++ src/Contador.java
@@ -0,0 +1,21 @@
+public class Contador { 
+	public static void main(String[] args) {
+        int parametroUm = 3;
+        int parametroDois = 7;
+            
+        System.out.println(parametroUm);
+        System.out.println(parametroDois);
+            
+        if(parametroUm >= parametroDois){ 
+                System.out.println(" O ParametroUm é maior que o ParametroDois");
+        }else{
+                System.out.println("O ParametroDois é maior que o ParametroUm");
+        }
+
+        int contar = parametroDois + parametroUm;{
+           System.out.println(contar); 
+        }
+    }
+}
+
+
